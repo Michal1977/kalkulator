693ffe0e79d2ce22de82c0713b47a57d55e39abd
Michał Jędrzejak <mjedrzejak77@gmail.com>
2022-03-23 18:44:22 +0100

    finished

---
+++ calculator.py
@@ -0,0 +1,29 @@
+import sys
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+action = int(input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:'))
+if action <= 4:
+    number1 = float(input('Podaj składnik 1.'))
+    number2 = float(input('Podaj składnik 2.'))
+
+else:
+    print("Zła wartość")
+    exit(1)
+
+if action == 1:
+    wynik = (number1)+(number2)
+    logging.info(f"Dodaję:", number1, number2)
+    print("Wynik to:", round(wynik,2))
+if action == 2:
+    wynik = (number1)-(number2)
+    logging.info(f"Odejmuję:", number1, number2)
+    print("Wynik to:", round(wynik,2))
+if action == 3:
+    wynik = (number1)*(number2)
+    logging.info(f"Mnożę:", number1, number2)
+    print("Wynik to:", round(wynik,2))
+if action == 4:
+    wynik = (number1)/(number2)
+    logging.info(f"Dzielę:", number1, number2)
+    print("Wynik to:", round(wynik,2))
